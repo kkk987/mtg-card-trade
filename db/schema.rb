3e8e413c8e9e248d361d2a65c6b69373e13d2ba3
kkk987 <markying21549@gmail.com>
2019-07-24 14:50:28 +1000

    fixed naming error

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 2019_07_24_044213) do
   enable_extension "plpgsql"
 
   create_table "sellers", force: :cascade do |t|
-    t.bigint "user_id_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_sellers_on_user_id_id"
+    t.index ["user_id"], name: "index_sellers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
