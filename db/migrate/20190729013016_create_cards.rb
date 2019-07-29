ced728a6382918f983cee7406e6dd362717564c5
lDoughnutl <sam_harvey98@live.com.au>
2019-07-29 11:33:44 +1000

    Created the Crad model and the Cards controller

---
+++ db/migrate/20190729013016_create_cards.rb
@@ -0,0 +1,15 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.string :title
+      t.string :type
+      t.string :color
+      t.string :rarity
+      t.integer :converted_mana_cost
+      t.string :set
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_121412) do
+ActiveRecord::Schema.define(version: 2019_07_29_013016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.string "color"
+    t.string "rarity"
+    t.integer "converted_mana_cost"
+    t.string "set"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
