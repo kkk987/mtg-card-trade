16e37808face70ad6fb482d691b035c13a81973e
lDoughnutl <sam_harvey98@live.com.au>
2019-07-30 10:22:07 +1000

    Added associations and some active storage stuff (Accidentally on master branch, I hope this doesn't ruin everything)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,7 @@
 class CardsController < ApplicationController
+    
+    @card.face.attach(params[:card] [:face])
+
     def index
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "trades#index"
   resources :trades, only: [:edit, :update]
-  resources :cards, only: [:index]
+  resources :cards, only: [:index, :show, ]
 end
