673fdf902ebe1a7a9bc2d477746443570b4b115c
kkk987 <markying21549@gmail.com>
2019-08-05 13:28:00 +1000

    fixed stripe checkout issues

---
+++ app/assets/javascripts/stripe_load.js
@@ -0,0 +1,4 @@
+// document.addEventListener("turbolinks:load", function() {
+//   var url=document.getElementById("https://js.stripe.com/v3/");
+//   setTimeout(function(){window.location = url;}, 3000);
+// })
